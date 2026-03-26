71e99c3748c4377bdae6a8f93a88ae1f67cb87cb
davidkogler <dkogler.code@gmail.com>
2026-03-26 11:26:23 +0100

    Add solution to exercise 10_leapYears
    Introduce two potential ways how to solve the problem. The first
    works but is not maintainable, the second one does the same but keeps
    maintainability in tact.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // although your code below works, you won't have any idea what it is doing later
+    // try to write it in a more readable way
+    //return (year % 4 === 0 && (!(year % 100 === 0) || year % 400 === 0)) ? true : false;
+    const isDivByFour = year % 4 === 0;
+    const isDivByHundred = year % 100 === 0;
+    const isDivByFourHundred = year % 400 === 0;
 
+    return (isDivByFour && (!isDivByHundred || isDivByFourHundred)) ? true : false;
 };
 
 // Do not edit below this line
