73f04a0ebe915a9f88d81d2e3c7f22f43813d269
davidkogler <dkogler.code@gmail.com>
2026-03-26 09:57:22 +0100

    Add solution to exercises 06 and 07
    Repeats string inputs in exercise 06.
    Reverses string inputs in exercise 07.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, reps) {
+    // return str.repeat(reps); --> Not allowed to fulfill this exercise, do manually
+    if (reps > 0) {
+        let addStr = str;
 
+        for (let i = 0; i < reps - 1; i++) {
+            str += addStr;
+        }
+        console.log(str);
+        return str;
+    } else if (reps < 0) {
+        return "ERROR";
+    } else return "";
 };
 
 // Do not edit below this line
--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let reverse = "";
 
+    for (i = text.length - 1; i >= 0; i--) {
+        reverse += text[i];
+    }
+
+    return reverse;
 };
 
 // Do not edit below this line
