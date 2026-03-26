6706b6129948c8a35bd2aaac8e48bc4ecbfafbe5
davidkogler <dkogler.code@gmail.com>
2026-03-26 09:53:22 +0100

    Add solution to exercise 08_removeFromArray
    Removes values (or arrays) from an array.
    Includes two possible solutions and explains why the use of the
    rest function parameter might be wiser than using arguments.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+// this works fine, but is not the best approach to do it as argument uses all 
+// of the supplied parameter (including arr)
+// const removeFromArray = function(arr) {
+//     const args = Array.from(arguments);
+//     return arr.filter(item => !args.includes(item));
+// };
+
+// therefore a more robust and direct way to do it would be to use 
+// the rest (...arg) parameter
+const removeFromArray = function(arr, ...args) {
+    return arr.filter(item => !args.includes(item));
 };
 
 // Do not edit below this line
