1b6d8506fb605c985caee58107254c4d67bfde9a
davidkogler <dkogler.code@gmail.com>
2026-05-11 08:00:13 +0200

    Add solution to 13_palindromes example
    Add logic to detect palindromes and return true if detected.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let cleanText = text
+                    .toLowerCase()
+                    .replaceAll(" ", "")
+                    .replaceAll(",", "")
+                    .replaceAll(".", "")
+                    .replaceAll("!", "")
+                    .toLowerCase();
+    let textArr = cleanText.split("");
+    let revText = textArr.reverse().join("");
+    return cleanText === revText;
 };
 
 // Do not edit below this line
