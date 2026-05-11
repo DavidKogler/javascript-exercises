fe7784b1c26c5e99dfca9aaeccbc8d4b9d338422
davidkogler <dkogler.code@gmail.com>
2026-05-11 08:51:27 +0200

    Add solution to 16_findTheOldest example
    Find the oldest person within an array using the array.sort()
    function and returning the first entry.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array.sort((a, b) => {
+            let currYear = new Date().getFullYear();
+            let aDeathYear = a.yearOfDeath ? a.yearOfDeath : currYear;
+            let bDeathYear = b.yearOfDeath ? b.yearOfDeath : currYear;
+            return (bDeathYear - b.yearOfBirth) - (aDeathYear - a.yearOfBirth)
+        });
+    return array[0];
 };
 
 // Do not edit below this line
