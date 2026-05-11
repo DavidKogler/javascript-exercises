f7660a5b14050f5ad4a30de791ee90024fbcbadc
davidkogler <dkogler.code@gmail.com>
2026-05-11 08:18:35 +0200

    Add solution to 14_fibonacci example
    Create fibonacci series using an array and return value at provided
    index.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    let memberNum = Number(member);
+    if (memberNum === 0) {
+        return 0;
+    } else if (memberNum < 0) {
+        return "OOPS";
+    } else {
+        let sequence = [1, 1];
+        for (i = 2; i < memberNum; i++) {
+            sequence.push(sequence[i - 2] + sequence[i - 1]);
+        }
+        return sequence[sequence.length - 1];
+    }
 };
 
 // Do not edit below this line
