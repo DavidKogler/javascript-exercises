c3ed03be74fcd64e00d11b8d9de311727b4341c7
davidkogler <dkogler.code@gmail.com>
2026-05-11 07:37:10 +0200

    Add solution to 12_calculator example
    Define the logic for every function in the javascript file.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+	return numArr.reduce((initialVal, currVal) => initialVal + currVal, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numArr) {
+  return numArr.reduce((product, currVal) => product * currVal);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fact = 1;
+  for (i = num; i > 0; i--) {
+    fact *= i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
